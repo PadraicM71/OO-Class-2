38fc69230bea49d5d2325c6c035002345cb0f2a1
PadraicM71 <padraic.moran@icloud.com>
2020-10-06 20:41:02 +0000

    wk2 x2

--- wk2.py
+++ wk2.py
@@ -3,5 +3,6 @@ list1.append(4)
 print (type(list1))
 print(list1[3])
 
-dict = {"name":"John Doe", age: 21, "net worth": 5213.4}
+dict = {"name":"John Doe", "age": 21, "net worth": 5213.4}
 print (dict)
+print(dict "name")
