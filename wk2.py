400a53feeb4434291c8839e57d3ef967443cdd74
PadraicM71 <padraic.moran@icloud.com>
2020-10-20 11:28:09 +0000

    werty

--- wk2.py
+++ wk2.py
@@ -1,8 +1,23 @@
+
+
+
+
+
+
 list1 = [1,2,3]
 list1.append(4)
 print (type(list1))
 print(list1[3])
 
+
+
 dict = {"name":"John Doe", "age": 21, "net worth": 5213.4}
+#essentially a JSON file - can pretty up if like on different lines.
 print (dict)
-print(dict "name")
+print(dict ["net worth"]) #refer to name of variable - whats inside sq brackets is a predicate
+
+
+
+
+
+
