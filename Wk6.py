667cb6367b3df4c86af39db70b290d5b6272ecfa
PadraicM71 <padraic.moran@icloud.com>
2020-11-03 18:53:02 +0000

    ws

---
+++ Wk6.py
@@ -0,0 +1,46 @@
+#wk6
+
+
+import math # Brings the whole math module in
+
+# print(math.sqrt(4)) # Prints the square root of 4, which is 2
+
+
+
+
+# from math import sqrt # brings in JUST the sqrt() function from the math module
+
+# print(sqrt(4)) # Prints the square root of 4, which is 2
+
+
+
+
+
+# # brings in JUST the sqrt() function from the math module and aliases it to square_root()
+# from math import sqrt as square_root
+
+# print(square_root(4)) # Prints the square root of 4, which is 2 
+
+
+
+from core import calc as super_func,
+iterate
+
+print(super_func(10,15))
+
+iterate()
+
+
+
+
+# def my_module_function():
+#     print("Hello from my_module.py")
+
+
+
+# from my_module import my_module_function # import my_module_function() from the my_module.py file
+
+# if __name__ == "__main__":
+#     my_module_function() # Run my_module_function() from my_module.py
+
+
