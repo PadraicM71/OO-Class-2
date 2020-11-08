c77741abf2ea6a6c8bc18ff9adb7fdb73f5d2179
PadraicM71 <padraic.moran@icloud.com>
2020-11-08 21:43:21 +0000

    test

---
+++ Test.py
@@ -0,0 +1,38 @@
+# # Test File
+
+
+# name = 'Callum'
+# print (name)
+
+# print (type(name))
+
+# lst = [1,2,'tree',True,34.25]
+# print (lst)
+# print(type(lst[3]))
+# lst.append(False)
+# print (lst)
+# print(type(lst[5]))
+
+# tst = input('Test Input: ')
+# print(tst)
+# print(type(tst))
+
+# di = {"name":"Callum","age":11,"Good?":True}
+# print(di)
+# print (di["name"])
+# di ["game"] = 59
+# print (di)
+
+
+# ok = int(tst)
+# print (ok)
+
+# x = int(input("say what?"))
+# print (x)
+
+
+s = "36"
+print(type(s))
+s = int(s)
+print(type(s))
+
