6b31a8e2c1f98e5385a48bded587cd55f350f94c
PadraicM71 <padraic.moran@icloud.com>
2020-11-10 20:51:08 +0000

    fgh

---
+++ Exam.py
@@ -0,0 +1,41 @@
+
+
+
+
+
+# num1 = input ("type in a number; ")
+
+# if (int(num1) % 2) == 0:
+#     print("number is even.")
+# else:
+#     print("number is odd.")
+
+
+
+num1 = input ("Start Number: ")
+num2 = input ("finish Number: ")
+
+for number in range(int(num1),int(num2)):
+    
+    if (number % 2) == 0:
+     print(number)
+
+
+
+# shopping_list = []
+
+# one = input("first item: ")
+# two = input("second item: ")
+# three = input("third item: ")
+# four = input("forth item: ")
+# five = input("fifth item: ")
+
+# shopping_list.append(one)
+# shopping_list.append(two)
+# shopping_list.append(three)
+# shopping_list.append(four)
+# shopping_list.append(five)
+
+# print (shopping_list)
+
+
