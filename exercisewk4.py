03b0a50f934df81365875667bf012905f714dd20
PadraicM71 <padraic.moran@icloud.com>
2020-11-10 00:09:40 +0000

    sdf

--- exercisewk4.py
+++ exercisewk4.py
@@ -12,13 +12,13 @@
         2. If 'Sausages' is the current element, stop iterating.
 """
 shopping_list = ["Bread", "Bannanas", "Pineapples", "Eggs", "Oranges", "Milk", "Sausages"]
-for item in shopping_list: # Iterate over the items in the shopping list
-    print(item)
-    if "Eggs" in item:
-        print ("Eggs detected - continuing")
-    elif "Sausages" in item:
-        print ("Sasuages detected - exit loop")
-        break
+# for item in shopping_list: # Iterate over the items in the shopping list
+#     print(item)
+#     if "Eggs" in item:
+#         print ("Eggs detected - continuing")
+#     elif "Sausages" in item:
+#         print ("Sasuages detected - exit loop")
+#         break
          # Remove this and replace with your own loop body logic
 
 
@@ -32,10 +32,15 @@ for item in shopping_list: # Iterate over the items in the shopping list
 #     them for input and append it to the list 3 times.
 # """
 # amount_to_add = int(input("How many items do you want to add?"))
-# counter = 0 # A counter to keep track of the number of items that have been added
+# counter = 1 # A counter to keep track of the number of items that have been added
 # while counter <= amount_to_add:
-#     # Do stuff
-#     None # Remove this and replace with your own loop body logic 
+#     amount_to_add -= 1
+#     r = input ("Item to add: ")
+#     shopping_list.append(r)
+    
+
+# print (shopping_list)
+
 
 
 
@@ -47,38 +52,52 @@ for item in shopping_list: # Iterate over the items in the shopping list
 #     The function takes a list and prints all 
 #     the items in the list.
 #     Bonus: Try using type declaration for the argument.
-# """
+# # """
 # def print_list(list_to_parse):
 #     """TODO: Add docstring"""
 #     for item in list_to_parse:
 #         print(item)
-# """
-#     =========== Exercise 2 =============   # Michael had a problem with this.
-#     Implement the delete_item() function
-#     below, all the details to do so should
-#     be there for you.
-#     Hint: you can use del(list[index]) to remove
-#     an item from a list.
-# """
-# def delete_item(list_to_parse, item_index):
-#     """Takes a list, removes an item at the 
-#     specified index and returns the list
-#     Parameters
-#     ----------
-#     list_to_parse:
-#         The list to remove the item from
-#     item_index:
-#         The index to remove an item from
-#     Returns
-#     -------
-#     The list with the item removed
-#     """
-#     # Do stuff here
+"""
+    =========== Exercise 2 =============   # Michael had a problem with this.
+    Implement the delete_item() function
+    below, all the details to do so should
+    be there for you.
+    Hint: you can use del(list[index]) to remove
+    an item from a list.
+"""
+
+print (shopping_list)
+
+
+def delete_item(list_to_parse, item_index):
+    """Takes a list, removes an item at the 
+    specified index and returns the list
+    Parameters
+    ----------
+    list_to_parse:
+        The list to remove the item from
+    item_index:
+        The index to remove an item from
+    Returns
+    -------
+    The list with the item removed
+    """
+    del(list_to_parse[item_index])
+
+
+
+delete_item(shopping_list,3)
+
+print (shopping_list)
+
+
+
+
 #     pass # This just tells python to do nothing; remove it when you add your code
 # shopping_list = ["eggs", "ham", "sausages"] # A test list to remove an item from
 # shopping_list = delete_item(shopping_list, 1) # Should remove 'ham' from the list
 # print(shopping_list)
 
 
-#
+
 
