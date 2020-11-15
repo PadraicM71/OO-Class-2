776618b5451ec0d6a5a9da03603869193f3a5613
PadraicM71 <padraic.moran@icloud.com>
2020-11-15 19:03:10 +0000

    pri

--- Test.py
+++ Test.py
@@ -18,29 +18,39 @@
 # print(p1.name)
 # print(p1.age)
 
-class People:
-    count = 0
-    def __init__(self, name ,age):
-        self.name = name
-        self.age = age
-        People.count += 1
-    def myfunc(self):
-        print("hello there " + "\nyour name is " + "\n" + self.name)
+# class People:
+#     count = 0
+#     def __init__(self, name ,age):
+#         self.name = name
+#         self.age = age
+#         People.count += 1
+#     def myfunc(self):
+#         print("hello there " + "\nyour name is " + "\n" + self.name)
 
 
 
-print (People.count)
+# print (People.count)
 
-k1 = People("qwerrty", 36)
-print (People.count)
-k2 = People("joe", 45)
-print (People.count)
-k3 = People("john", 56)
+# k1 = People("qwerrty", 36)
+# print (People.count)
+# k2 = People("joe", 45)
+# print (People.count)
+# k3 = People("john", 56)
 
-print (People.count)
+# print (People.count)
 
-print(k1.name)
-k1.myfunc()
+# print(k1.name)
+# k1.myfunc()
 
+number = int(input("Please type a number: "))
+x = 2
+while True:
+  num1 = (number%x)
+  print (num1)
+  if num1 == 0:
+    print("Number is composite")
+    break
 
-
+  elif x == number:
+    print("Number is prime")
+    x += 1
