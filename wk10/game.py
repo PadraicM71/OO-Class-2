5f77b702ceb4db4b123cee3f92e02ff1a685402a
PadraicM71 <padraic.moran@icloud.com>
2020-12-01 19:31:42 +0000

    fghj

--- wk10/game.py
+++ wk10/game.py
@@ -1,14 +1,15 @@
 
 
-game_list = [0,1,2]
+# game_list = [0,1,2]
 
-def display_game(game_list):
-    print("Here is the current list")
-    print(game_list)
+# def display_game(game_list):
+#     print("Here is the current list")
+#     print(game_list)
 
-display_game(game_list)
+# display_game(game_list)
 
-#------------------------------------------
+
+#-----------------------------------------------
 
 from os import system, name 
 # define our clear function 
@@ -19,20 +20,42 @@ def clear():
     # for mac and linux(here, os.name is 'posix') 
     else: 
         _ = system('clear') 
-game_list = [0,1,2]
 def display_game(game_list):
     print("Here is the current list")
     print(game_list)
-display_game(game_list)
 def position_choice():
     choice = 'wrong'
     while choice not in ['0','1','2']:
         choice = input("Pick a position to replace (0,1,2): ")
         if choice not in['0','1','2']:
             clear()
-            print("Sorry, but you did not choose a valid position (0,1,2)")
+            print("Sorry, but you did not choose a valid position (0,1,2): ")
     return int(choice)
-
+def replacement_choice(game_list,position):
+    user_placement = input("Type a string to place at the position: ")
+    game_list[position] = user_placement
+    return game_list
+def gameon_choice():
+    choice = "wrong"
+    while choice not in ['Y','N']:
+        choice = input("Would you like to keep playing? Y or N ")
+        if choice not in ['Y','N']:
+            clear()
+            print("Sorry, I didn't understand. Please make sure to choose Y or N. ")
+    if choice == "Y":
+        return True
+    else:
+        return False
+game_on = True
+game_list = [0,1,2]
+while game_on:
+    clear()
+    display_game(game_list)
+    position = position_choice()
+    game_list = replacement_choice(game_list,position)
+    clear()
+    display_game(game_list)
+    game_on = gameon_choice()
 
 
 #-----------------------------------------------
