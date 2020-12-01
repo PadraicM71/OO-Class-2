be59cac72f0a6436632fe2f016bdcd3e43f89773
PadraicM71 <padraic.moran@icloud.com>
2020-12-01 18:22:30 +0000

    gfhj

---
+++ wk10/wk10.py
@@ -0,0 +1,38 @@
+
+
+# def display_list(mylist):
+#     print(mylist)
+
+# mylist = [0,1,2,3,4,5,6,7,8,9,10]
+# display_list(mylist)
+
+# result = input("Please enter a number: ")
+
+# print(result)
+
+# print(type(result))
+
+# print(int(result))
+
+
+#------------------------------
+
+
+# def user_choice():
+#     '''
+#     User inputs a number (0-10) and we return this in integer form.
+#     No parameter is passed when calling this function.
+#     '''
+#     choice = input("Please input a number (0-10):")
+
+#     return int(choice)
+
+# print (type(user_choice()))
+
+
+#-----------------------------
+
+some_input = '10'
+
+# Lot's of .is methods availble on string
+print(some_input.isdigit())
